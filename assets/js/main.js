7199d15e86466c292c8a8fd1747ba687ebbca9e6
HD111995 <shomawmisho1234@gmail.com>
2021-07-26 09:58:10 +0200

    Second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 function confirm() {
     console.log("hi")
-    document.getelementById('cap').innerHTML = "YOUR EMAIL HAS BEEN CONFIRMED.<br>THANKS FOR SUBSCRIBING!"
+    document.getElementById("cap").innerHTML = "&#10004;<br>YOUR EMAIL HAS BEEN CONFIRMED.<br>THANKS FOR SUBSCRIBING!"
+    document.getElementById("cap").style.fontSize = "46px"
+    document.getElementById("para").innerHTML = "Now we just need to confirm your email adress.please click the link in the email we sent you.Thanks!"
+    document.getElementById("em").style.display = "none"
+    document.getElementById("sub").value = "BACK TO DESIGNERCHALNGE.COM"
+    document.getElementById("sub").style.width = "30vw"
+    document.getElementById("sub").style.height = "3vw"
+    document.getElementById("sub").style.marginLeft = "35vw"
+    document.getElementById("sub").style.backgroundColor = "#3C30FF"
 }
